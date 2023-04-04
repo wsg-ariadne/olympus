c39efcacc1bffdd0f76cfe2218a18343469b04a5
Jared Dantis <jareddantis@gmail.com>
2023-04-04 21:00:34 +0800

    Install Auth0 SDK

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createAuth0 } from '@auth0/auth0-vue'
 
 import App from './App.vue'
 import router from './router'
@@ -8,5 +9,14 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(
+  createAuth0({
+    domain: "jareddantis.jp.auth0.com",
+    clientId: "iCJYH757i97NZUK3woYJeKnWld9LK5zj",
+    authorizationParams: {
+      redirect_uri: window.location.origin
+    }
+  })
+);
 
 app.mount('#app')
