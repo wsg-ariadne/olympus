a7a509579b05f12e1b2fa8e4aee1d2655f09c9ae
Jared Dantis <jareddantis@gmail.com>
2023-04-04 23:43:15 +0800

    components: Use Flowbite's button instead

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav
-    class="w-full h-16 sticky top-0 left-0 flex items-center justify-center bg-white"
+    class="w-full h-16 sticky top-0 left-0 flex items-center justify-center"
   >
     <div class="mx-4 flex items-center justify-between max-w-screen-md w-full">
       <img
@@ -11,9 +11,9 @@
 
       <div v-if="isAuthenticated">
         <p>Hello, {{ user.given_name }}</p>
-        <NiceButton compact round mood="error" @click="logout">Log out</NiceButton>
+        <Button pill color="red" @click="logout">Log out</Button>
       </div>
-      <NiceButton compact round @click="login">Log in</NiceButton>
+      <Button pill @click="login">Log in</Button>
     </div>
   </nav>
 </template>
@@ -21,12 +21,12 @@
 <script>
 import { defineComponent } from 'vue'
 import { useAuth0 } from '@auth0/auth0-vue'
-import NiceButton from '@/components/NiceButton.vue'
+import { Button } from 'flowbite-vue' 
 
 export default defineComponent({
   name: 'Navbar',
   components: {
-    NiceButton
+    Button
   },
   setup() {
     const { loginWithRedirect, user, isAuthenticated, logout } = useAuth0()
