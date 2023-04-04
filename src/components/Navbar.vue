1443aed633e422d14ab3a042cab0a7dd822b2cea
Jared Dantis <jareddantis@gmail.com>
2023-04-05 00:11:59 +0800

    components/Navbar: Properly show user name

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav
-    class="w-full h-16 sticky top-0 left-0 flex items-center justify-center"
+    class="w-full h-16 sticky z-10 top-0 left-0 flex items-center justify-center bg-white border border-b-gray-200"
   >
     <div class="mx-4 flex items-center justify-between max-w-screen-md w-full">
       <img
@@ -9,11 +9,11 @@
         class="h-4"
       >
 
-      <div v-if="isAuthenticated">
-        <p>Hello, {{ user.given_name }}</p>
+      <div v-if="isAuthenticated" class="flex items-center">
+        <p class="mr-4">Hello, <strong>{{ user.given_name }}</strong></p>
         <Button pill color="red" @click="logout">Log out</Button>
       </div>
-      <Button pill @click="login">Log in</Button>
+      <Button pill @click="login" v-else>Log in</Button>
     </div>
   </nav>
 </template>
